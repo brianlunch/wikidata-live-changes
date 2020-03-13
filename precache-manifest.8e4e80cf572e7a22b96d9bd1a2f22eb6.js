de9abff522d502fc805577ac4bc86a864ea3f9b2
brianlunch <blynch5@tcd.ie>
2020-03-13 19:51:59 +0000

    Updates

---
+++ precache-manifest.8e4e80cf572e7a22b96d9bd1a2f22eb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cfe0ccb5389c7d24bd3571cbe5c7c9",
+    "url": "/wikidata-live-changes/index.html"
+  },
+  {
+    "revision": "ad458ebf2f40e1f30ddb",
+    "url": "/wikidata-live-changes/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "323225fd658e63b1857e",
+    "url": "/wikidata-live-changes/static/css/main.8c0551e2.chunk.css"
+  },
+  {
+    "revision": "ad458ebf2f40e1f30ddb",
+    "url": "/wikidata-live-changes/static/js/2.49b4cefc.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/wikidata-live-changes/static/js/2.49b4cefc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "323225fd658e63b1857e",
+    "url": "/wikidata-live-changes/static/js/main.4600b9a0.chunk.js"
+  },
+  {
+    "revision": "d8f4c14554234d0f2388",
+    "url": "/wikidata-live-changes/static/js/runtime-main.c834f876.js"
+  }
+]);
